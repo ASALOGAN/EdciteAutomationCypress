49bb0bd6cc0659d57c347c0f7ee7aca152dc56c3
AHMED SHOIEB AQHTAR <89434245+ASALOGAN@users.noreply.github.com>
2023-06-24 17:44:01 +0530

    Update Assignment.cy.js

--- Assignment.cy.js
+++ Assignment.cy.js
@@ -1,6 +1,8 @@
+//This uses a custom command to log in you just need to enter the username and password
+
 describe('Edcite Master Assignment', () => {
   it('create Assignment', () => {
-    cy.login('username', 'password'); // enter username and pass word
+    cy.login('username', 'password'); // enter username and password
     const today = new Date();
     const name = today.toLocaleString('en-US', { month: 'long', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric' });
     cy.createAssignment(name);
